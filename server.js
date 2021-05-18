bd6772b4e27d72955e157a3322d06b19dd5e66df
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-05-18 02:10:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const colors = require('colors')
+const morgan = require('morgan')
+const dotenv = requires('dotenv')
 
+dotenv.config({ path: './routes/transactions' })
+
+const app = express()
+
+app.use('/API/v1/transactions', transactions)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port:${PORT}`.blue.bold))
